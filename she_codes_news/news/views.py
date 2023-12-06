180893cabc9c1a855df155a739e136e620b42f25
lindi <lindi.harding@gmail.com>
2023-12-06 23:55:24 +0800

    ordered stories by date and assigned image url feild to storycard

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -14,11 +14,11 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         '''Return all news stories.'''
-        return NewsStory.objects.all()
+        return NewsStory.objects.all().order_by('-pub_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all()[:4]
+        context['latest_stories'] = NewsStory.objects.all().order_by('-pub_date')[:4]
         return context
 
 class StoryView(generic.DetailView):
