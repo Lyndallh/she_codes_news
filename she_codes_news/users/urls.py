864356c547c3e8c2f0ff1e96b7a503c5f6511e78
lindi <lindi.harding@gmail.com>
2023-12-16 03:06:43 +0800

    Added profile to users app

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, AccountView
 
 app_name ='users'
 
 urlpatterns = [
-    path('create-account/', CreateAccountView.as_view(),
-    name='createAccount'),
+    path('create-account/', CreateAccountView.as_view(), name='createAccount'),
+    path('<int:pk>', AccountView.as_view(), name='profile'),
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -2,10 +2,27 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.views import generic
-
+from news.models import NewsStory, Comment
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+
+
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
+
+
+class AccountView(generic.DetailView):
+    model = CustomUser
+    template_name = 'users/profile.html'
+    context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_stories'] = NewsStory.objects.filter(author=self.kwargs['pk']).order_by('-pub_date')
+        context['user_comments'] = Comment.objects.filter(author=self.kwargs['pk']).order_by('-date')
+        return context
+    
+
+
